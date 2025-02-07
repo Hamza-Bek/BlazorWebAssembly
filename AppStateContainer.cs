a79a478a3ba7f59fa1ee3240b48ef03b4782506f
Hamza Bek <hamzamrbek@gmail.com>
2025-02-07 19:12:05 +0200

    Add AppState file

---
+++ AppStateContainer.cs
@@ -0,0 +1,14 @@
+namespace MasteringBlazor;
+
+public class AppStateContainer
+{
+    public int CurrentCounter { get; set; }
+    
+    public Action<int>? OnCounterChanged { get; set; }
+
+    public void UpdateCounter(int newCounter)
+    {
+        CurrentCounter = newCounter;
+        OnCounterChanged?.Invoke(newCounter);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,4 +13,6 @@ builder.Services.AddScoped<IBookService, BookService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddSingleton<AppStateContainer>();
+
 await builder.Build().RunAsync();
